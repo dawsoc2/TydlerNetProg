9162294b9436e87427bbaffea123192048a960e3
Chandler Xerxes Dawson <dawsoc2@hotmail.com>
2017-04-12 16:12:48 -0400

    Signal Handling
    Removed the for() loop for the base variable because it's exceedingly unnecessary.
    
    Added in signal handling so when timeout is used with the program it exits gracefully, giving a final count.
    (to use timeout you do "timeout -s 10 *S* mpiexec -np *P* ./a.out" where *S* is the signal to send and *P* is the number of processes. On ubuntu you send signal 10 for SIGUSR1)
    
    was there anything else I had to do?

--- hw3/mpi_primes.c
+++ hw3/mpi_primes.c
@@ -1,8 +1,17 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
-#define LIMIT 1000000
+#define LIMIT 100000000
+
+int end_now = 0;
+
+void sig_handler(int signo) {
+	if (signo == SIGUSR1) {
+		end_now = 1;
+	}
+}
 
 int isprime(int n) {
 	int i;
@@ -13,13 +22,15 @@ int isprime(int n) {
 }
 
 int main (int argc, char *argv[]) {
-	int count, id, n, pcount, primesum, base;
+	int count, id, n, nsum, pcount, primesum, base;
 	double start_time, end_time;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &count);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
+	signal(SIGUSR1, sig_handler);
+	
 	start_time = MPI_Wtime();
 	pcount = 0;
 
@@ -28,15 +39,22 @@ int main (int argc, char *argv[]) {
 	// (id - count) exists to allow space for all processes to reduce at 10
 	// even if they do not check a number between 1 and 10
 
+	base = 10;
 	for (n = ((id - count) * 2) + 1; n <= LIMIT; n += count * 2) {
+		if (end_now == 1) {
+			MPI_Reduce(&pcount, &primesum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+			MPI_Reduce(&n, &nsum, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+			if (id == 0)
+				printf("Primes below %d: %d\n", nsum, primesum);
+			break;
+		}
 		if (n > 0 && isprime(n))
 			pcount++;
-		for (base = 10; base <= LIMIT; base *= 10) {
-			if (n < base && n > base - count * 2) {
-				MPI_Reduce(&pcount, &primesum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-				if (id == 0)
-					printf("Primes below %d: %d\n", base, primesum);
-			}
+		if (n < base && n > base - count * 2) {
+			MPI_Reduce(&pcount, &primesum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+			base = base*10;
+			if (id == 0)
+				printf("Primes below %d: %d\n", base, primesum);
 		}
 	}
 
@@ -48,4 +66,3 @@ int main (int argc, char *argv[]) {
 	MPI_Finalize();
 	return 0;
 }
-
